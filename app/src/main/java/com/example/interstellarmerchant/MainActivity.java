0db1c0584d662ebd0d1c88596ed0136cf75aac8f
Dalong <dalonghu777@gmail.com>
2019-02-14 18:54:00 -0500

    changed layout files

--- app/src/main/java/com/example/interstellarmerchant/MainActivity.java
+++ app/src/main/java/com/example/interstellarmerchant/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.interstellarmerchant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner difficultySpinner;
+
+    public enum Difficulties {
+        Beginner,
+        Easy,
+        Normal,
+        Hard,
+        Impossible;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
+
+        ArrayAdapter<Difficulties> difficulties = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Difficulties.values());
+        difficulties.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(difficulties);
+
     }
 }
