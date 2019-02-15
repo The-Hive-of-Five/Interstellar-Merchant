72d4ce0e9447eda424ffd50388dccbc31b09fb26
Dalong <dalonghu777@gmail.com>
2019-02-15 17:44:16 -0500

    made initial button changing code

--- app/src/main/java/com/cs2340/interstellarmerchant/MainActivity.java
+++ app/src/main/java/com/cs2340/interstellarmerchant/MainActivity.java
@@ -2,8 +2,12 @@ package com.cs2340.interstellarmerchant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.cs2340.interstellarmerchant.model.player.game_config.Difficulty;
 import com.example.interstellarmerchant.R;
@@ -23,5 +27,21 @@ public class MainActivity extends AppCompatActivity {
         difficulties.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         difficultySpinner.setAdapter(difficulties);
 
+        Button continueBtn = (Button) findViewById(R.id.continue_btn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText pilotSkillPts = (EditText) findViewById(R.id.pilot_skillpts);
+                EditText fighterSkillPts = (EditText) findViewById(R.id.fighter_skillpts);
+                EditText traderSkillPts = (EditText) findViewById(R.id.trader_skillpts);
+                EditText engineerSkillPts = (EditText) findViewById(R.id.engineer_skillpts);
+                TextView result = (TextView) findViewById(R.id.char_setup_result);
+
+                int pilotP = Integer.parseInt(pilotSkillPts.getText().toString());
+                int fighterP = Integer.parseInt(fighterSkillPts.getText().toString());
+                int traderP = Integer.parseInt(traderSkillPts.getText().toString());
+                int engineerP = Integer.parseInt(engineerSkillPts.getText().toString());
+            }
+        });
     }
 }
