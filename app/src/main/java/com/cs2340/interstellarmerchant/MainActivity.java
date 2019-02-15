60606d9d2ec1bb79124d2304143195eaff8abf5e
Dalong <dalonghu777@gmail.com>
2019-02-15 17:12:27 -0500

    changed constraints

--- app/src/main/java/com/cs2340/interstellarmerchant/MainActivity.java
+++ app/src/main/java/com/cs2340/interstellarmerchant/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.laeyout.activity_main);
+        setContentView(R.layout.activity_main);
 
         difficultySpinner = findViewById(R.id.difficulty_spinner);
 
