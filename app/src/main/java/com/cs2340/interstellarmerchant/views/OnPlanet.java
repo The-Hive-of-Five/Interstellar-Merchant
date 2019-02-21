94c226080a19df31dd0156c0007a00b46fbdbe08
Tanmoy Panigrahi <tanmoypanigrahi@Tanmoys-MacBook-Pro.local>
2019-02-20 22:38:06 -0500

    added onplanet screen

---
+++ app/src/main/java/com/cs2340/interstellarmerchant/views/OnPlanet.java
@@ -0,0 +1,17 @@
+package com.cs2340.interstellarmerchant.views;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.cs2340.interstellarmerchant.model.player.Player;
+import com.cs2340.interstellarmerchant.R;
+import com.cs2340.interstellarmerchant.utilities.LogUtility;
+
+public class OnPlanet extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onplanet);
+    }
+}
