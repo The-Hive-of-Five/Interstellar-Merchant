8ab13b88404693d91e9d743aa78c37384b0996c2
Dalong <dalonghu777@gmail.com>
2019-02-20 22:09:09 -0500

    further shop buy changes

--- app/src/main/java/com/cs2340/interstellarmerchant/views/MarketBuyRecyclerViewAdapter.java
+++ app/src/main/java/com/cs2340/interstellarmerchant/views/MarketBuyRecyclerViewAdapter.java
@@ -1,7 +1,9 @@
 package com.cs2340.interstellarmerchant.views;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -10,12 +12,26 @@ import android.widget.TextView;
 
 import com.cs2340.interstellarmerchant.R;
 
+import java.util.ArrayList;
+
 public class MarketBuyRecyclerViewAdapter extends RecyclerView.Adapter<MarketBuyRecyclerViewAdapter.ViewHolder>{
     private static final String TAG = "MarketBuyRecyclerViewAd";
+
+    private ArrayList<String> itemNames = new ArrayList<>();
+    private ArrayList<String> itemPrices = new ArrayList<>();
+    private Context itemContext;
+
+    public MarketBuyRecyclerViewAdapter(ArrayList<String> itemNames, ArrayList<String> itemPrices, Context itemContext) {
+        this.itemNames = itemNames;
+        this.itemPrices = itemPrices;
+        this.itemContext = itemContext;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.market_shop_item, viewGroup, false);
+
         return null;
     }
 
