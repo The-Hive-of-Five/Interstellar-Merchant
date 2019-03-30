0c2eaf143c4638e27887f2f1d798c964fa71a359
Dalong <dalonghu777@gmail.com>
2019-03-29 23:06:55 -0400

    added popup window for save

---
+++ app/src/main/java/com/cs2340/interstellarmerchant/views/SavePop.java
@@ -0,0 +1,47 @@
+package com.cs2340.interstellarmerchant.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import com.cs2340.interstellarmerchant.R;
+
+public class SavePop extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.save_pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width * 0.8), (int)(height * 0.5));
+
+        Button yes = (Button) findViewById(R.id.save_yes);
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SavePop.this, Savegame.class));
+            }
+        });
+
+        Button no = (Button) findViewById(R.id.save_no);
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SavePop.this, Savegame.class));
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/cs2340/interstellarmerchant/views/Savegame.java
+++ app/src/main/java/com/cs2340/interstellarmerchant/views/Savegame.java
@@ -6,17 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.cs2340.interstellarmerchant.model.player.Player;
 import com.cs2340.interstellarmerchant.R;
 import com.cs2340.interstellarmerchant.utilities.LogUtility;
+
 import java.util.ArrayList;
 
 public class Savegame extends AppCompatActivity{
+
     private ArrayList<String> gameNames = new ArrayList<>();
     private ArrayList<String> gameTimes = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.savegame);
+
+        Button save = (Button) findViewById(R.id.save_button);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Savegame.this, SavePop.class));
+            }
+        });
     }
 }
