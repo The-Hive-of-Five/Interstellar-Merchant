aaea014fc2dd9527a2394f35e16d2fd062205387
TanmoyPanigrahi <tanmoy.panigrahi2@gmail.com>
2019-04-01 13:41:46 -0400

    Added all of the view methods with fixed names and times

--- app/src/main/java/com/cs2340/interstellarmerchant/views/Loadgame.java
+++ app/src/main/java/com/cs2340/interstellarmerchant/views/Loadgame.java
@@ -1,7 +1,10 @@
 package com.cs2340.interstellarmerchant.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.cs2340.interstellarmerchant.R;
 
@@ -9,11 +12,40 @@ import java.util.ArrayList;
 
 public class Loadgame extends AppCompatActivity{
 
-    private ArrayList<String> gameNames = new ArrayList<>();
-    private ArrayList<String> gameTimes = new ArrayList<>();
+    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<String> times = new ArrayList<>();
 
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loadgame);
+
+        initLoadGames();
     }
+
+    private void initLoadGames() {
+        names.add("Alice");
+        times.add("15:20");
+
+        names.add("Bob");
+        times.add("14:20");
+
+        names.add("Claire");
+        times.add("9:20");
+
+        initRecyclerView();
+    }
+
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_load);
+        LoadRecyclerViewAdapter adapter =
+                new LoadRecyclerViewAdapter(names, times, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+
+
 }
